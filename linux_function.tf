1141bc12ad308cd6156f4e2957114c854f08576e
Dwijesh Mishra <60146522+DwijeshMishra@users.noreply.github.com>
2023-03-31 18:45:00 +0530

    Update linux_function.tf

--- linux_function.tf
+++ linux_function.tf
@@ -17,3 +17,23 @@ resource "azurerm_linux_function_app" "example" {
   app_service_plan_id = azurerm_app_service_plan.service_plan.id
   app_settings        = var.app_settings
 }
+
+resource "azurerm_service_plan" "example" {
+  name                = "example-app-service-plan"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  os_type             = "Linux"
+  sku_name            = "Y1"
+}
+
+resource "azurerm_linux_function_app" "example" {
+  name                = "example-linux-function-app"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+
+  storage_account_name       = azurerm_storage_account.example.name
+  storage_account_access_key = azurerm_storage_account.example.primary_access_key
+  service_plan_id            = azurerm_service_plan.example.id
+
+  site_config {}
+}
