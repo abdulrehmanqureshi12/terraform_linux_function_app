1b9473ec0de95354ef7e98e5608851819f38e8c0
Dwijesh Mishra <60146522+DwijeshMishra@users.noreply.github.com>
2023-04-05 14:08:17 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,47 +1,30 @@
-variable "app_service_plan_name" {
-  type        = string
-  description = "Name of the App Service Plan"
-  default     = "my-app-service-plan"
+# Define variables
+variable "resource_group_name" {
+  type    = string
+  default = "my-resource-group"
 }
 
-variable "app_service_plan_tier" {
-  type        = string
-  description = "Tier of the App Service Plan"
-  default     = "Dynamic"
+variable "storage_account_name" {
+  type    = string
+  default = "testaccountsub"
 }
 
-variable "app_service_plan_size" {
-  type        = string
-  description = "Size of the App Service Plan"
-  default     = "Y1"
+variable "container_registry_name" {
+  type    = string
+  default = "mycontainerregistrydeepak"
 }
 
-variable "app_name" {
-  type        = string
-  description = "Name of the Function App"
-  default     = "my-function-app"
+variable "function_app_name" {
+  type    = string
+  default = "myfunctionappdeepak"
+}
+
+variable "service_plan_name" {
+  type    = string
+  default = "myserviceplan"
 }
 
 variable "location" {
-  type        = string
-  description = "Location for the App Service Plan and Function App"
-  default     = "eastus"
-}
-
-variable "resource_group_name" {
-  type        = string
-  description = "Name of the Resource Group"
-  default     = "my-resource-group"
-}
-
-variable "app_settings" {
-  type        = map(string)
-  description = "App settings for the Function App"
-  default = {
-  app_settings = {
-    WEBSITE_RUN_FROM_PACKAGE        = "0"
-    WEBSITE_CONTAINER_IMAGE_NAME    = "docker.io/library/hello-world:latest"
-    FUNCTIONS_WORKER_RUNTIME        = "node"
-    WEBSITES_ENABLE_APP_SERVICE_STORAGE = "true"
-  }
+  type    = string
+  default = "eastus"
 }
