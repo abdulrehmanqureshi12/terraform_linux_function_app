e5bd1c96489e3bbb05880bfd0e63afd7eec2704d
Abdul Rehman Qureshi <60146522+abdulrehmanqureshi12@users.noreply.github.com>
2023-05-23 17:36:48 +0530

    Update bastion.tf

--- bastion.tf
+++ bastion.tf
@@ -1,3 +1,4 @@
+#approach 1
 resource "azurerm_bastion_host" "bastion" {
   count = data.azurerm_bastion_host.existing_bastion.sku == "Standard" ? 1 : 0
 
@@ -20,5 +21,28 @@ resource "azurerm_bastion_host" "bastion" {
         create_before_destroy = true
       }
     }
+  }
+}
+#approach 2
+locals {
+  recreate_bastion = data.azurerm_bastion_host.existing_bastion.sku == "Standard" ? [0, 1] : []
+}
+
+resource "azurerm_bastion_host" "bastion" {
+  for_each              = local.recreate_bastion
+  name                  = "shared-bastion"
+  location              = var.location
+  resource_group_name   = var.target_infra_rg
+  sku                   = "Basic"
+
+  ip_configuration {
+    name                  = "configuration"
+    subnet_id             = azurerm_subnet.subnet-bastion.id
+    public_ip_address_id  = azurerm_public_ip.bastion.id
+  }
+
+  lifecycle {
+    prevent_destroy         = true
+    create_before_destroy   = true
   }
 }
