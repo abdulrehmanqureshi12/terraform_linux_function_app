fe60b9b72e3c45dc752789ff7c88ebc919f3734c
Abdul Rehman Qureshi <60146522+abdulrehmanqureshi12@users.noreply.github.com>
2023-05-24 01:06:34 +0530

    Update bastion.tf

--- bastion.tf
+++ bastion.tf
@@ -1,72 +1,18 @@
-#approach 1
-resource "azurerm_bastion_host" "bastion" {
-  count = data.azurerm_bastion_host.existing_bastion.sku == "Standard" ? 1 : 0
 
-  dynamic "bastion" {
-    for_each = count.index == 0 ? [1] : []
-
-    content {
-      name                = "shared-bastion"
-      location            = var.location
-      resource_group_name = var.target_infra_rg
-      sku                 = "Basic"
-
-      ip_configuration {
-        name                  = "configuration"
-        subnet_id             = azurerm_subnet.subnet-bastion.id
-        public_ip_address_id  = azurerm_public_ip.bastion.id
-      }
-
-      lifecycle {
-        create_before_destroy = true
-      }
-    }
-  }
-}
-#approach 2
 locals {
-  recreate_bastion = data.azurerm_bastion_host.existing_bastion.sku == "Standard" ? [0, 1] : []
+  recreate_bastion = data.azurerm_bastion_host.existing_bastion.sku == "Standard"
 }
 
-resource "azurerm_bastion_host" "bastion" {
-  for_each              = local.recreate_bastion
+resource "azurerm_bastion_host" "example" {
+  for_each            = local.recreate_bastion ? toset(["0", "0"]) : toset(["0"])
   name                  = "shared-bastion"
   location              = var.location
   resource_group_name   = var.target_infra_rg
   sku                   = "Basic"
 
   ip_configuration {
-    name                  = "configuration"
-    subnet_id             = azurerm_subnet.subnet-bastion.id
-    public_ip_address_id  = azurerm_public_ip.bastion.id
-  }
-
-  lifecycle {
-    prevent_destroy         = true
-    create_before_destroy   = true
-  }
-}
-
-#apprach 3
-resource "azurerm_bastion_host" "bastion" {
-  dynamic "bastion" {
-    for_each = data.azurerm_bastion_host.existing_bastion.sku == "Standard" ? [1] : []
-
-    content {
-      name                  = "shared-bastion"
-      location              = var.location
-      resource_group_name   = var.target_infra_rg
-      sku                   = "Basic"
-
-      ip_configuration {
-        name                  = "configuration"
-        subnet_id             = azurerm_subnet.subnet-bastion.id
-        public_ip_address_id  = azurerm_public_ip.bastion.id
-      }
-
-      lifecycle {
-        create_before_destroy = true
-      }
-    }
+    name                 = "configuration"
+    subnet_id            = azurerm_subnet.example.id
+    public_ip_address_id = azurerm_public_ip.example.id
   }
 }
